a0a27eea76aff996c9a39b0434ce1f8b9576681e
Jakub Kisielewski <kisielewski.kb@gmail.com>
2019-09-30 21:51:41 +0200

    [ci] Test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
-    social: {
-      twitter: `kylemathews`,
-    },
+    title: `Devscord.pl Blog`,
+    author: `Devscord.pl`,
+    description: `The beginnings of devscord.pl`,
+    siteUrl: `https://devscord.pl/`
   },
   plugins: [
     {
@@ -68,8 +65,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Devscord.pl Blog`,
+        short_name: `DevscordPL`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
