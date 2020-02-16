3821c530f411897888a9265bfdb2fd971efd3e64
Mateusz Stepaniuk <stepek75@gmail.com>
2020-02-16 21:04:11 +0100

    Test netlify build

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,7 +8,7 @@ export default function NotFoundPage({ location, data }) {
   return (
     <Layout location={location} title={getTitleFromData(data)}>
       <SEO title="404: Not Found" />
-      <h1>Not Found</h1>
+      <h1>Not Found!</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </Layout>
   )
